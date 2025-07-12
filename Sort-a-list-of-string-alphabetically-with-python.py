e36c3564fde161ddf137ef86d94f2b61ca2b52a8
Ali Najafi <76534541+Ali9721@users.noreply.github.com>
2025-07-12 09:03:16 -0700

    Sort-a-list-of-string-alphabetically-with-python
    1. Define "strings" as a variable to get strings & "split".
    
    2. Use "sort" for sorting string "a-z"
    
    3. Output the result.

---
+++ Sort-a-list-of-string-alphabetically-with-python.py
@@ -0,0 +1,8 @@
+# Define "strings" as a variable to get strings & "split".
+strings = input("Please enter strings separated by spaces: ").split()
+
+# Use "sort" for sorting string "a-z"
+strings.sort()
+
+# Output the result.
+print("Sorted strings: " , strings)
